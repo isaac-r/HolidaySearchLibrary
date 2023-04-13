80f55a65faf2979c7ac2e244f148a39e9e6419e8
isaac.richardson <isaacr828@gmail.com>
2023-04-13 13:30:06 +0100

    Adding package model

---
+++ HolidaySearchLibrary/Models/Package.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearchLibrary.Models
+{
+    public class Package
+    {
+        public Flight Flight { get; set; }
+        public Hotel Hotel { get; set; }
+        public int TotalPrice
+        {
+            get
+            {
+                return Flight.Price + (Hotel.PricePerNight * Hotel.Nights);
+            }
+        }
+    }
+}
